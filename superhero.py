7596ab78a348df922de767b6aca3e1e848f9dd45
rjowell20007 <44178512+rjowell@users.noreply.github.com>
2022-01-24 13:45:51 +0000

    Initial commit

---
+++ superhero.py
@@ -0,0 +1,36 @@
+class superhero:
+
+  def fly(self):
+    print("flying now")
+
+def recharge(self):
+  print("I'm coming back to life")
+  
+  def fightEnemy(self):
+    print("punch kick")
+
+
+
+
+
+
+
+
+
+
+# This thing is called what?
+
+'''
+HOW CAN WE CUSTOMIZE OUR SUPERHERO?
+Special Moves
+Costume
+Name
+Height
+Hair Color
+The Way They Are Built
+Body Type
+Species - Human/Cyborg
+Number of Hit Points
+weakensses
+
+'''
